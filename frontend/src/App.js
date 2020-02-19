a3e987a589a4adf45e3435c8b14ce969555461e2
MarkKragerup <markido@live.dk>
2020-02-19 20:59:42 +0100

    test complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React DOOOCKEEER
+          Learn React ...ugh....
         </a>
       </header>
     </div>
