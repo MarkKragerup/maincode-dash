4050953d55c9dd75159fb93276666d5c22e693ae
MarkKragerup <markido@live.dk>
2020-02-21 22:55:10 +0100

    Tooltips

--- frontend/src/components/DashboardLink/DashboardLink.js
+++ frontend/src/components/DashboardLink/DashboardLink.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './DashboardLink.css';
 import { Tooltip } from 'antd';
 
-export default function DashboardLink({tabIndex, href, className="", children}){
+export default function DashboardLink({tooltip="Go to this dashboard!", tabIndex, href, className="", children}){
     return (
-        <a className={`theme-card link-card ${className}`} href={href} tabIndex={tabIndex}>
-            <div className="left-block">
-                {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
-                <h2 className="fas fa-external-link-alt" />
-            </div>
-            <div className="right-block">
-                {children}
-            </div>
-        </a>
+        <Tooltip title={tooltip}>
+            <a className={`theme-card link-card ${className}`} href={href} tabIndex={tabIndex}>
+                <div className="left-block">
+                    {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
+                    <h2 className="fas fa-external-link-alt" />
+                </div>
+                <div className="right-block">
+                    {children}
+                </div>
+            </a>
+        </Tooltip>
     );
 }
