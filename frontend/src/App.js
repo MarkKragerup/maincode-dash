7ca470ad117601e306a1bd61003985ce016b6e5a
MarkKragerup <markido@live.dk>
2020-02-21 23:45:16 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,7 @@ export default function App() {
         <div className="App">
             <Frontpage/>
 
-            <h2 style={{display: 'block'}}>Still needs:</h2>
-            <ul>
-                <li>Go to cloud.com link</li>
-                <li>SSH copy button</li>
-                <li>View on github link</li>
-                <li>Complete rework for in-app running</li>
-                <li>Application version by electron</li>
-            </ul>
+
         </div>
     );
 }
