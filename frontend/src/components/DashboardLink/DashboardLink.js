08915436c123a713a370b644d9e37780499fefb4
MarkKragerup <markido@live.dk>
2020-02-21 22:48:00 +0100

    Loads of changes

--- frontend/src/components/DashboardLink/DashboardLink.js
+++ frontend/src/components/DashboardLink/DashboardLink.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './DashboardLink.css';
+import { Tooltip } from 'antd';
 
-export default function DashboardLink(){
+export default function DashboardLink({tabIndex, href, className="", children}){
     return (
-        <div>
-            <p>hej</p>
-        </div>
+        <a className={`theme-card link-card ${className}`} href={href} tabIndex={tabIndex}>
+            <div className="left-block">
+                {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
+                <h2 className="fas fa-external-link-alt" />
+            </div>
+            <div className="right-block">
+                {children}
+            </div>
+        </a>
     );
 }
--- frontend/src/pages/Frontpage/Frontpage.js
+++ frontend/src/pages/Frontpage/Frontpage.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import './Frontpage.css';
 
+import DashboardLink from "../../components/DashboardLink/DashboardLink";
+
 export default function Frontpage(){
 
     useEffect(() => {
@@ -9,18 +11,13 @@ export default function Frontpage(){
 
     return (
         <div className="link-cards-container">
-            <a className="theme-card link-card" href='https://google.dk' tabIndex={1}>
-                <div className="left-block">
-                    <h2 className="fas fa-external-link-alt" />
-                </div>
-                <div className="right-block">
-                    <h2>Advanced Monitoring</h2>
-                    <p>Some server info will be coming.</p>
-                    <p>test2</p>
-                    <p>test3</p>
-                </div>
-            </a>
-
+            <DashboardLink
+                tabIndex={1}
+                href={"https://monitor.maincode.dk"}
+            >
+                <h2>Advanced Monitoring</h2>
+                <p>xx xxx xxx</p>
+            </DashboardLink>
             <a className="theme-card link-card" tabIndex={2}>
                 <div className="left-block">
                     <h2 className="fas fa-external-link-alt" />
